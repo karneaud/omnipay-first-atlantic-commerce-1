a85b397e1c2eee61a453bc27f5a0f62dd14bb921
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-07-26 12:52:57 -0400

    Create recurring trait.
    Create additional parameters for Recurring details of transaction

---
+++ src/Traits/RecurringTrait.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Omnipay\FirstAtlanticCommerce\Traits;
+
+trait RecurringTrait
+{
+    public function setExecutionDate($value)
+    {
+        return $this->setParameter('ExecutionDate', $value);
+    }
+
+    public function getExecutionDate()
+    {
+        return $this->getParameter('ExecutionDate');
+    }
+
+    public function setIsRecurring(bool $value = false)
+    {
+        return $this->setParameter('isRecurring', $value );
+    }
+
+    public function getIsRecurring()
+    {
+        return $this->getParameter('isRecurring');
+    }
+
+    public function setFrequency($value = 'M')
+    {
+        return $this->setParameter('Frequency', $value);
+    }
+
+    public function getFrequency()
+    {
+        return $this->getParameter('Frequency');
+    }    
+    
+    public function setNumberOfRecurrences(integer $value)
+    {
+        return $this->setParameter('NumberOfRecurrences', $value);
+    }
+
+    public function getNumberOfRecurrences()
+    {
+        return $this->getParameter('NumberOfRecurrences');
+    }
+}
